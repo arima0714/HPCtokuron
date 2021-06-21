5555325cd601503dd53b308bdbb6af733dab6470
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-06-21 10:19:42 +0900

    対数を対数計算をせずに算出できることを知らせるコードを追記した

--- float2bin.c
+++ float2bin.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(void)
 {
@@ -8,6 +9,13 @@ int main(void)
   i = *((int *)&f);
   printf("float=%16.8e\n",f);
   printf("int=%08x\n",i);
+
+  i = 0x52300000;
+  f = *((float *)&i);
+  printf("int=%08x\n",i);
+  printf("float=%16.8e\n",f);
+
+  printf("log2=%f\n", log(f)/log(2));
 
   return 0;
 }
